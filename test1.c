c93b8422e79b3ee8abc64365f9779261cb15d1fe
lain893ac88f0ed1c9 <you@example.com>
2019-03-03 12:20:14 -0500

    main callback function works correctly

--- test1.c
+++ test1.c
@@ -1,23 +1,91 @@
 #include <stdio.h>
 #include <pcap.h>
+#include "sniff.h"
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header,
+    const u_char *packet);
 
 int main(int argc, char *argv[])
 {
-    char *dev = argv[1];
-    char errbuf[400];
-
     pcap_t *handle;
-    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    char *dev = "wlp4s0";
+    char errbuf[PCAP_ERRBUF_SIZE];
+    struct bpf_program fp;
+    char filter_exp[] = "port 80";
+    bpf_u_int32 mask;
+    bpf_u_int32 net;
+    struct pcap_pkthdr header;
+    const unsigned char *packet;
+    struct sniff_ip test;
 
+    if(pcap_lookupnet(dev, &net, &mask, errbuf) == -1)
+    {
+        fprintf(stderr, "Can't get netmask for device %s\n", dev);
+        net = 0;
+        mask = 0;
+    }
+
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
     if(handle == NULL)
-        puts(errbuf);
+    {
+        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+        return 2;
+    }
 
     if(pcap_datalink(handle) != DLT_EN10MB)
     {
-        fprintf(stderr, "Device %s doesn't provide ethernet headers, not supported\n", dev);
-        return(2);
+        fprintf(stderr, 
+            "Device %s doesn't provide ethernet headers, not supported\n", dev);
+        return 2;
     }
 
+    if(pcap_compile(handle, &fp, filter_exp, 0, net) == -1)
+    {
+        fprintf(stderr, 
+            "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return 2;
+    }
 
-    return 0;
+    if(pcap_setfilter(handle, &fp) == -1)
+    {
+        fprintf(stderr,
+            "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));   
+        return 2;
+    }
+
+    pcap_loop(handle, -1, got_packet, NULL);
+
+    pcap_close(handle);
+}
+
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header,
+    const u_char *packet)
+{
+    printf("Recieved packet of size %u!\n", header->caplen);
+    /* ethernet headers are always exactly 14 bytes */
+    #define SIZE_ETHERNET 14
+
+    const struct sniff_ethernet *ethernet; /* The ethernet header */
+    const struct sniff_ip *ip; /* The IP header */
+    const struct sniff_tcp *tcp; /* The TCP header */
+    const char *payload; /* Packet payload */
+
+    u_int size_ip;
+    u_int size_tcp;
+
+    ethernet = (struct sniff_ethernet*)(packet);
+    ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if (size_ip < 20) {
+        printf("   * Invalid IP header length: %u bytes\n", size_ip);
+        return;
+    }
+    tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp)*4;
+    if (size_tcp < 20) {
+        printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+        return;
+    }
+    payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
 }
